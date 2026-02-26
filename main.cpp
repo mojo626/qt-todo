8290b1b117328971cb0d74997488b687f7196ef2
mojo626 <benjibearjaynes@gmail.com>
2026-02-26 01:04:30 -0800

    created qml application

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 
-#include <QtCore>
-#include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQuick/QtQuick>
+#include <QQmlContext>
 
-int main() {
-    
-    std::cout << "Qt version: " << qVersion() << std::endl;
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load("qrc:/main.qml");
+    if(engine.rootObjects().isEmpty())
+        return EXIT_FAILURE;
+
+    return app.exec();
 }
